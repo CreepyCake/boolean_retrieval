25e1a5eb3a8c538a6c82ee2da1e40df38bd9adc7
creepycake <zombiefaraday@gmail.com>
2016-02-23 18:58:51 +0300

    Comments

--- parser.rb
+++ parser.rb
@@ -1,6 +1,7 @@
 require "unicode"
 class String
 
+  #methods for enabling unicode downcase and upcase
   def downcase
     Unicode::downcase(self)
   end
@@ -20,6 +21,8 @@ class String
     self.replace capitalize
   end
 
+  #methods for deciding if word is from initial text: it must contain only cyrillic symbols,
+  #or only latin symbols or it must be a number
   def sanitize
     self.split('').collect do |token|
       token.gsub(/[^-<>A-Za-zА-Яа-я0-9]/, '')
@@ -42,6 +45,7 @@ class String
 
 end
 
+#class that makes all the work
 class InvertedIndex
 
   attr_accessor :args, :data, :doc_number, :token_sum, :token_number
@@ -56,6 +60,7 @@ class InvertedIndex
     @index_filename = index_filename
   end
 
+  #if we already have an index file, than we can just reopen it
   def open_index
     if File.exist? @index_filename
       @data = Marshal.load open(@index_filename)
@@ -64,22 +69,24 @@ class InvertedIndex
     end
   end
 
+  #parse words and write to hash pairs <word, document number>
   def parse
     @args.each do |filename|
       open(filename) do |file|
         file.read.split.each do |word|
-          if word.include? "Section"
+          if word.include? "Section" #beginning or end of a text
             @in_text = !@in_text
           end
-          if word.include? "</p>"
+          if word.include? "</p>" #we break texts into documents that are located between </p> tags
             @doc_number += 1
           end
           @token_sum += word.length
           @token_number += 1
           if @in_text and word.length >= 3
-            word = word.downcase.sanitize.join.regulars
-            @data[word] ||= []
-            @data[word] << @doc_number unless @data[word].include? @doc_number
+            word = word.downcase.sanitize.join.regulars #process the word
+            @data[word] ||= [] #we either have some record in hash with that key or we don't
+            @data[word] << @doc_number unless @data[word].include? @doc_number #don't make record
+                                                   # if we already met this word in this document
           end
         end
       end
@@ -87,6 +94,7 @@ class InvertedIndex
     end
   end
 
+  #marshal index into file
   def write_index
     open(@index_filename, "w") do |index|
       index.write Marshal.dump(@data)
@@ -105,15 +113,14 @@ start = Time.now
 
 inverted_index = InvertedIndex.new(ARGV, "index.dat")
 inverted_index.create_inverted_index
+term_sum = inverted_index.data.keys.inject(0) {|sum, key| sum + key.length} #for calculation of
+                                                                           # the average term length
 
 finish = Time.now
-
-@test = Marshal.load open("index.dat")
-term_sum = @test.keys.inject(0) {|sum, key| sum + key.length}
 
 puts "Documents = #{inverted_index.doc_number}"
 puts "Tokens = #{inverted_index.token_number}"
 puts "Terms = #{inverted_index.data.length}"
 puts "Average length of tokens = #{inverted_index.token_sum.to_f / inverted_index.token_number.to_f}"
-puts "Average length of terms = #{term_sum.to_f / @test.length.to_f}"
+puts "Average length of terms = #{term_sum.to_f / inverted_index.data.length.to_f}"
 puts "Elapsed time = #{finish-start} seconds"
